939d8bcabbe80139a82081c96c2fc6a5f303fd01
Sourabhpro <148372308+Sourabhpro@users.noreply.github.com>
2023-12-06 12:48:08 +0530

    Update Used car SQL queries.sql

--- Used car SQL queries.sql
+++ Used car SQL queries.sql
@@ -4,12 +4,12 @@ SELECT * FROM car_dekho;
 -- What is the total number of used car listings? (Count of total records) --
 SELECT count(*) from car_dekho
 -- 7927 --
--- how many cars are available from the year 2023? --
+-- How many cars are available from the year 2023? --
 SELECT count(*) FROM car_dekho 
 WHERE year = 2023;
 -- 6 Car from the year 2023 --
 
--- How many cars available form the yaear 2020, 2021 and 2022 --
+-- How many cars available form the yaear 2020, 2021 and 2022? --
 SELECT count(*) FROM car_dekho 
 WHERE year in (2020,2021,2022)   #7 #7 #74
 GROUP BY year
@@ -29,15 +29,25 @@ SELECT year, Count(*) FROM car_dekho
 WHERE fuel = "petrol" 
 GROUP BY year;
 
--- list of petrol cars names that have been driven for less than 30000 kms from the year 2015 to 2020 --
+-- List of petrol cars names that have been driven for less than 30000 kms from the year 2015 to 2020 --
 
 SELECT name, year, km_driven
 FROM car_dekho
 WHERE km_driven < 30000 AND fuel = "petrol" AND year BETWEEN 2015 and 2020;
 ORDER BY km_driven;
 
--- list of petrol cars names that have been driven for less than 30000 kms from the year 2015 to 2020 having seatig capacity more than 4--
+-- List of petrol cars names that have been driven for less than 30000 kms from the year 2015 to 2020 having seatig capacity more than 4--
 SELECT name, year, km_driven, seats
 FROM car_dekho
 WHERE km_driven < 30000 AND fuel = "petrol" AND year BETWEEN 2015 and 2020 AND seats > 4;
 ORDER BY km_driven;
+
+-- Which years have the more than 100 cars available? --
+SELECT year, Count(*) 
+FROM car_dekho 
+GROUP BY year having count(*)>100;
+
+-- Which years have the less than 100 cars available? --
+SELECT year, Count(*) 
+FROM car_dekho 
+GROUP BY year having count(*)<100;
